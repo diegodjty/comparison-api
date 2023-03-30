abd044b346f131952d532ac6ebafe176f673c524
Diego <diegotaveras2@outlook.com>
2023-03-30 00:04:44 -0400

    add dark mode, notifications, navbar, dropdown menu and more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Paragraph from "../components/ui/Paragraph";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Paragraph size={"default"} />
+    </main>
+  );
 }
---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import { FC, useState } from "react";
+import Button from "./ui/Button";
+import { toast } from "./ui/Toast";
+
+interface SignInButtonProps {}
+
+const SingInButton: FC<SignInButtonProps> = ({}) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const signInWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn("google");
+    } catch (error) {
+      toast({
+        title: "Error signing in",
+        message: "Please try again later",
+        type: "error",
+      });
+    }
+  };
+
+  return (
+    <Button onClick={signInWithGoogle} isLoading={isLoading}>
+      Sing In
+    </Button>
+  );
+};
+
+export default SingInButton;

9eace7a82da1c6a228411329d7c7fb597acd8eb0
Diego <diegotaveras2@outlook.com>
2023-03-31 01:27:21 -0400

    documentation page

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import LargeHeading from "@/ui/LargeHeading";
+
+import type { Metadata } from "next";
+import Paragraph from "@/ui/Paragraph";
+import DocumentationTabs from "@/components/DocumentationTabs";
+import "simplebar-react/dist/simplebar.min.css";
+
+export const metadata: Metadata = {
+  title: "Comparison API | Documentation",
+  description: "Free & open-source text similarity API",
+};
+
+const page: FC = () => {
+  return (
+    <div className="container max-w-7xl mx-auto mt-12">
+      <div className="flex flex-col items-center gap-6">
+        <LargeHeading>Making a request</LargeHeading>
+        <Paragraph>api/v1/comparison</Paragraph>
+
+        <DocumentationTabs />
+      </div>
+    </div>
+  );
+};
+
+export default page;

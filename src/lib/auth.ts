d7fb6288c1120c76a1784b7f21d9b3c93adbc7dd
Diego <diegotaveras2@outlook.com>
2023-04-01 13:06:09 -0400

    add table component and create api functionality

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,19 +1,19 @@
-import { NextAuthOptions } from "next-auth";
-import { db } from "./db";
+import { db } from "@/lib/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-function getGoogleCredential() {
+function getGoogleCredentials(): { clientId: string; clientSecret: string } {
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-
   if (!clientId || clientId.length === 0) {
-    throw new Error("No clientID for google provider set");
+    throw new Error("Missing GOOGLE_CLIENT_ID");
   }
 
   if (!clientSecret || clientSecret.length === 0) {
-    throw new Error("No ClientSecret for google provider set");
+    throw new Error("Missing GOOGLE_CLIENT_SECRET");
   }
+
   return { clientId, clientSecret };
 }
 
@@ -27,18 +27,19 @@ export const authOptions: NextAuthOptions = {
   },
   providers: [
     GoogleProvider({
-      clientId: getGoogleCredential().clientId,
-      clientSecret: getGoogleCredential().clientSecret,
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
     }),
   ],
   callbacks: {
-    session({ token, session }) {
+    async session({ token, session }) {
       if (token) {
         session.user.id = token.id;
         session.user.name = token.name;
         session.user.email = token.email;
         session.user.image = token.picture;
       }
+
       return session;
     },
     async jwt({ token, user }) {
@@ -47,10 +48,12 @@ export const authOptions: NextAuthOptions = {
           email: token.email,
         },
       });
+
       if (!dbUser) {
         token.id = user!.id;
         return token;
       }
+
       return {
         id: dbUser.id,
         name: dbUser.name,
